865c2d6b48ff22ef7d479ac29d4c0402d97fa95d
Eric <50396876+Guerrero-Eric@users.noreply.github.com>
2019-11-30 21:10:15 -0700

    Add files via upload

--- weather_api/script.js
+++ weather_api/script.js
@@ -1,96 +1,43 @@
-var cityName = "";
-
-// SELECT ELEMENTS
-const iconElement = document.querySelector(".weather-icon");
-const tempElement = document.querySelector(".temperature-value p");
-const descElement = document.querySelector(".temperature-description p");
-const locationElement = document.querySelector(".location p");
-const notificationElement = document.querySelector(".notification");
-
-// App data
-const weather = {};
-
-weather.temperature = {
-    unit : "celsius"
-}
-
-// APP CONSTS AND VARS
-const KELVIN = 273;
-// API KEY
-const key = "82005d27a116c2880c8f0fcb866998a0";
-
-// CHECK IF BROWSER SUPPORTS GEOLOCATION
-if('geolocation' in navigator){
-    navigator.geolocation.getCurrentPosition(setPosition, showError);
-}else{
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = "<p>Browser doesn't Support Geolocation</p>";
-}
-/*
-// SET USER'S POSITION
-function setPosition(position){
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
+function makeApiCall( url, callback ){
+    var xhr = new XMLHttpRequest();
+  
+    xhr.addEventListener( 'load', function(){
+      var response = JSON.parse(xhr.responseText);
+      if ( callback ){
+        callback( response );
+      }
+    });
+  
+    xhr.open( 'GET', url );
+    xhr.send();
+  }
+  
+  makeApiCall( 'https://freegeoip.app/json/', function( data ){
+    document.getElementById( 'city' ).innerHTML = data.city + ', ' + data.region_name;  
     
-    getWeather(latitude, longitude);
-}
-*/
-// SHOW ERROR WHEN THERE IS AN ISSUE WITH GEOLOCATION SERVICE
-function showError(error){
-    notificationElement.style.display = "block";
-    notificationElement.innerHTML = `<p> ${error.message} </p>`;
-}
-
-function getWeather(cityName){
-    // API KEY 
-    const key = "ef4d047f0140df5f44f2c48843408179";
-
-    var api = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&units=metric&apikey="+key;  //API Request Link
-
-    
-    fetch(api)
-        .then(function(response){
-            let data = response.json();
-            return data;
-        })
-        .then(function(data){
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-        })
-        .then(function(){
-            displayWeather();
-        });
-}
-
-// DISPLAY WEATHER TO UI
-function displayWeather(){
-    iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-}
-
-// C to F conversion
-function celsiusToFahrenheit(temperature){
-    return (temperature * 9/5) + 32;
-}
-
-// WHEN THE USER CLICKS ON THE TEMPERATURE ELEMENET
-tempElement.addEventListener("click", function(){
-    if(weather.temperature.value === undefined) return;
-    
-    if(weather.temperature.unit == "celsius"){
-        let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
-        fahrenheit = Math.floor(fahrenheit);
-        
-        tempElement.innerHTML = `${fahrenheit}°<span>F</span>`;
-        weather.temperature.unit = "fahrenheit";
-    }else{
-        tempElement.innerHTML = `${weather.temperature.value}°<span>C</span>`;
-        weather.temperature.unit = "celsius"
-    }
-});
-
+    var latitude = data.latitude;
+    var longitude = data.longitude;
+  
+    makeApiCall( 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=imperial&APPID=ef4d047f0140df5f44f2c48843408179', function( data ){
+      var weather = data.weather[0].main;
+      var weatherDescription = data.weather[0].description;
+      document.getElementById( 'weather' ).innerHTML = weatherDescription;
+  
+      loadBackground( latitude, longitude, weather );
+    });  
+  });
+  
+  function loadBackground( latitude, longitude, weather ) {
+    var script_element = document.createElement('script');
+  
+    script_element.src = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=f68d91290adbddc4257c093376306448&lat=' + latitude + '&lon=' + longitude + '&accuracy=1&tags=' + weather + '&sort=relevance&extras=url_l&format=json';
+  
+    document.getElementsByTagName('head')[0].appendChild( script_element );
+  }
+  
+  function jsonFlickrApi( data ){
+    var photo = data.photos.photo[0];
+    document.querySelector( 'body' ).style.backgroundImage = 'url("' + photo.url_l + '")';
+  }
+  
+  
